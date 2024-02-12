7369d7e90dea8e15182ed49f6ff2a951903a91e3
francisco oliveira <fs9457489@gmail.com>
2024-02-12 13:28:17 -0300

    treinando JavaScript para Back-End

---
+++ practicing-v4.js
@@ -0,0 +1,36 @@
+// Formas de declarar variáveis: var, let e const
+
+/// var altura = 5
+// ar comprimento = 7
+
+// area = altura * comprimento
+// console.log(area)
+// var area
+
+
+// let forma = 'retângulo'
+// let altura = 5
+// let comprimento = 7
+// let area
+
+// if (forma === 'retângulo') {
+//     area = altura * comprimento
+// }else {
+//    area = (altura * comprimento) / 2
+// }
+
+//console.log(area)
+
+
+const forma = 'triângulo'
+const altura = 5
+const comprimento = 7
+let area
+
+if (forma === 'quadrado') {
+  area = altura * comprimento
+} else {
+  area = (altura * comprimento) / 2
+}
+
+console.log(area)
